317c7ab0ac18f2e06c5d41bba9bc6aa688c313dc
Ahriana <rosiecode@outlook.com>
2020-11-16 14:42:40 +0000

    Add project files.

---
+++ UnityDebug/Class1.cs
@@ -0,0 +1,49 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Harmony;
+
+namespace UnityDebug {
+	public class UnityDebugClass: MelonMod {
+		[HarmonyPatch(typeof(Debug), "Log", typeof(Il2CppSystem.Object))]
+		class UnityDebugClass_Debug_Log__Object {
+			static void Prefix (Il2CppSystem.Object message) {
+				MelonLogger.Log(message.ToString());
+			}
+		}
+
+		[HarmonyPatch(typeof(Debug), "Log", typeof(Il2CppSystem.Object), typeof(UnityEngine.Object))]
+		class UnityDebugClass_Debug_Log__Object_Object {
+			static void Prefix (Il2CppSystem.Object message, UnityEngine.Object context) {
+				MelonLogger.Log($"[{context}]: {message}");
+			}
+		}
+
+		[HarmonyPatch(typeof(Debug), "LogWarning", typeof(Il2CppSystem.Object))]
+		class UnityDebugClass_Debug_LogWarning__Object {
+			static void Prefix (Il2CppSystem.Object message) {
+				MelonLogger.LogWarning(message.ToString());
+			}
+		}
+
+		[HarmonyPatch(typeof(Debug), "LogWarning", typeof(Il2CppSystem.Object), typeof(UnityEngine.Object))]
+		class UnityDebugClass_Debug_LogWarning__Object_Object {
+			static void Prefix (Il2CppSystem.Object message, UnityEngine.Object context) {
+				MelonLogger.LogWarning($"[{context}]: {message}");
+			}
+		}
+
+		[HarmonyPatch(typeof(Debug), "LogError", typeof(Il2CppSystem.Object))]
+		class UnityDebugClass_Debug_LogError__Object {
+			static void Prefix (Il2CppSystem.Object message) {
+				MelonLogger.LogError(message.ToString());
+			}
+		}
+
+		[HarmonyPatch(typeof(Debug), "LogError", typeof(Il2CppSystem.Object), typeof(UnityEngine.Object))]
+		class UnityDebugClass_Debug_LogError__Object_Object {
+			static void Prefix (Il2CppSystem.Object message, UnityEngine.Object context) {
+				MelonLogger.LogError($"[{context}]: {message}");
+			}
+		}
+	}
+}
---
+++ UnityDebug/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+using MelonLoader;
+using UnityDebug;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnityDebug")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("re-enable unity debug logs")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnityDebug")]
+[assembly: AssemblyCopyright("Copyright © Ahriana 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: MelonInfo(typeof(UnityDebugClass), "UnityDebug", "1.0.0.0", "Ahriana")]
+[assembly: MelonGame(null, null)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c9f22d22-e95e-4a37-b16e-65340b4ccf38")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
